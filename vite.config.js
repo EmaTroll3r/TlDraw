3bf4fc88622871838f8e98568242f52ca1ca8e27
Emanuele Costanzo <emanuelecostanzo03@gmail.com>
2025-03-11 14:54:23 +0100

    server config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  server: {
+    host: true, // Ascolta su tutte le interfacce di rete
+    port: 8888,
+    allowedHosts: ['affiuccio.duckdns.org'] // Aggiungi l'host consentito
   }
 });
